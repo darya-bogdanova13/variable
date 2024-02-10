97b8b0f4ea384259a9a1c7c2632e05b8c371d56b
darya <darya.kolycheva98@yandex.ru>
2024-02-10 06:42:13 +0300

    Variables and actions with them

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box=box*3;
+        System.out.println(box);
+        box=box/4;
+        System.out.println(box);
+    }
+}
