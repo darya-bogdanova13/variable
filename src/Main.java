132c77118ce6790d3f1cbd0b66dbcc8b7f6a7cd4
darya <darya.kolycheva98@yandex.ru>
2024-02-10 07:37:37 +0300

    Variables and actions with them

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,41 @@ public class Main {
         System.out.println(box);
         box=box/4;
         System.out.println(box);
+
+        var lifttingCopacity=50;
+        var stuffWeight=20;
+        var copacityLeft= lifttingCopacity-stuffWeight;
+        System.out.println ("Еще можно положить" + copacityLeft+"кг вещей");
+
+        var appleWeight=2;
+        var orangesWeight=3;
+        var frutWeight=appleWeight+orangesWeight;
+        System.out.println("Общий вес фруктов" +frutWeight);
+
+        var meatWeight=4;
+        var waterWeight=5;
+        var tomatoesWeight=2;
+        var cucumberWeight=2;
+        var pepperWeight= 2;
+        var zucchiniWeight= 2;
+        var vegetableWeight = tomatoesWeight+cucumberWeight+pepperWeight+zucchiniWeight;
+        var productsWeight = frutWeight+vegetableWeight+waterWeight+meatWeight;
+        System.out.println ("Общий вес продуктов"+productsWeight+"кг!");
+
+        var leftWeight = lifttingCopacity-stuffWeight-productsWeight;
+        System.out.println("Места осталось" + leftWeight + "кг!");
+
+        productsWeight = productsWeight * 2;
+        System.out.println("Теперь вес продуктов" + productsWeight);
+
+        leftWeight = lifttingCopacity - stuffWeight - productsWeight;
+        System.out.println("Теперь места осталось " + leftWeight + "кг!");
+
+        var overload = (stuffWeight + productsWeight) % lifttingCopacity;
+        System.out.println("Перегруз на " + overload + "кг!");
+
+        var productsInOneCar = productsWeight / 2;
+        System.out.println("Продуктов в обной машине теперь " + productsInOneCar);
+
     }
 }
